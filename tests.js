093fbe3f4100343f135ceef5382a0cf8e1b9d3e6
alu0100811933 <alu0100811933@ull.edu.es>
2016-03-16 18:40:39 +0000

    Añadiendo tests

--- tests.js
+++ tests.js
@@ -2,8 +2,13 @@ var expect = chai.expect;
 
 describe("Medida", function() {
   describe("constructor", function() {
-    it("Deberia dar valor a los atributos ", function() {
+    it("Deberia dar valor a los atributos (tipo, valor) ", function() {
       var aux = new Medida("32", "c");
+      expect(aux.value).to.equal("32");
+      expect(aux.type).to.equal("c");
+    });
+    it("Deberia crear el objeto sin tipo ", function() {
+      var aux = new Medida("32c");
       expect(aux.value).to.equal("32");
       expect(aux.type).to.equal("c");
     });
@@ -12,5 +17,23 @@ describe("Medida", function() {
     it("Deberia convertir de F a C", function() {
       expect(Medida.convertir("32f to c")).to.equal("0.00 Celsius");
     });
+    it("Deberia dar error al pasar a una temperatura desconocida", function() {
+      expect(Medida.convertir("32f to f")).to.equal('Desconozco como convertir desde "f" hasta "f"');
+    });
+    it("Deberia dar error al pedir una conversion erronea", function() {
+      expect(Medida.convertir("32f a k")).to.equal('Introduzca una temperatura valida: 32.6e-8 F to C');
+    });
   });
 });
+/*
+describe("Conversor", function() {
+  describe("Funciones de conversion", function() {
+    it("Deberia convertir de Celsius a Kelvin y a Farenheit ", function() {
+      Celsius cel = new Celsius(0);
+      expect(cel.toFarenheit()).to.equal(32);
+    //  expect(aux.type).to.equal("c");
+    });
+
+  });
+});
+*/
