2141e0c120b28feac1b5d45258e50a8e4b809d2a
alu0100821338 <alu0100821338@ull.edu.es>
2016-03-16 17:53:15 +0000

    Añadiendo Blanket

--- tests.js
+++ tests.js
@@ -1,10 +1,16 @@
 var expect = chai.expect;
 
 describe("Medida", function() {
-  describe("convertir", function() {
+  describe("constructor", function() {
+    it("Deberia dar valor a los atributos ", function() {
+      var aux = new Medida("32", "c");
+      expect(aux.value).to.equal("32");
+      expect(aux.type).to.equal("c");
+    });
+  });
+  describe("#convertir", function() {
     it("Deberia convertir de F a C", function() {
       expect(Medida.convertir("32f to c")).to.equal("0.00 Celsius");
     });
-
   });
 });
